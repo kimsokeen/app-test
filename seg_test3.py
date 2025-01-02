7feba7540f2f925bbaafa7bb7a300283de4f0171
kimsokeen <keenmeka@gmail.com>
2025-01-02 17:22:40 +0700

    Update seg_test3.py

--- seg_test3.py
+++ seg_test3.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import sqlite3
-import tensorflow
 from datetime import datetime
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
